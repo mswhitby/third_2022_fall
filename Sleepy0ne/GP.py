e833e6e280fefb87a80b17e5069d698ffb5b5594
Sleepy0ne <112113369+Sleepy0ne@users.noreply.github.com>
2022-08-25 13:29:37 -0500

    The first lines at the beginning

--- Sleepy0ne/GP.py
+++ Sleepy0ne/GP.py
@@ -1,1 +1,9 @@
+#The start
+name = input("Welcome, To begin. Please enter a Name. Let be known you will use this name all throught the game. ")
+yesorno = input(name + ", Are you sure this is the name you want? (Can't go back after you decide!)")
+if "yes" in yesorno:
+  print("Then let's begin.")
+else:
+    name2 = input("Enter name: ")
+    print(name2 + ", Hopefully this is what yu wanted. Let's begin." )
 
